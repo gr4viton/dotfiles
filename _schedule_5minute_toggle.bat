5524ddfba623df4f69bebf8998112049aecacff5
gr4viton <lordmutty@gmail.com>
2015-11-04 14:12:10 +0100

    st 04.11.2015 14:12:10,72

--- _schedule_5minute_toggle.bat
+++ _schedule_5minute_toggle.bat
@@ -1,7 +1,7 @@
 schtasks /query /tn gr4log_git_update_minute 
 IF %ERRORLEVEL% EQU 1 (
     ECHO aa
-    schtasks /create /tn gr4log_git_update_minute /tr D:\LOG\gr4log\_gr4log_git_update.bat /sc minute /mo 5
+    schtasks /create /tn gr4log_git_update_minute /tr D:\LOG\gr4log\_gr4log_git_update.bat /sc minute /mo 5 /V1
     ECHO Task gr4log_git_update_minute scheduled succesfully
 ) ELSE (
     ECHO Task gr4log_git_update_minute already scheduled, proceeding to delete the task.
