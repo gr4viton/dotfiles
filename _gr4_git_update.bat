88a095393db9841d32ec4f1b8bf8b52b5653a02b
gr4viton <lordmutty@gmail.com>
2015-12-02 12:42:24 +0100

    st 02.12.2015 12:42:24,95

---
+++ _gr4_git_update.bat
@@ -0,0 +1,19 @@
+echo Current working directory is %cd%
+cd %~dp0
+echo Changing direcotory to %cd%
+
+git.exe add .
+git.exe commit -m "%date% %time%"
+git.exe pull -v --no-rebase --progress "origin"
+git.exe commit -m "%date% %time% after pull"
+git.exe push origin master
+
+python.exe -c "print('\7')" :: BEEP
+
+set /a time2wait=%1	:: from first parameter
+echo %time2wait%
+if %time2wait% EQU 0 (
+	timeout 3
+) ELSE (
+	timeout %time2wait%
+)
