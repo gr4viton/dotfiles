0d45ee736b93ab909c2f1a2d20967ada2d29396c
gr4viton <lordmutty@gmail.com>
2016-08-24 10:38:11 +0200

    %date% %time% from xubuntu

---
+++ gr4log_git_update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#git config --global credential.helper 'cache --timeout=28800'
+
+git add .
+git commit -m "%date% %time% from xubuntu"
+echo $SSH_AUTH_SOCK
+git pull -v --no-rebase --progress "origin"
+git commit -m "%date% %time% from xubuntu after pull"
+git push origin master
+
+# beep
+python -c "print('\7')" 
+
+
+#exit()
