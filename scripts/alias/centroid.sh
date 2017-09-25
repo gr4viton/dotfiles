8b5535b081aabb63e940d73ed594d37f620e37dd
gr4viton <lordmutty@gmail.com>
2017-09-25 20:22:59 +0200

    %date% %time% from xubuntu

--- scripts/alias/centroid.sh
+++ scripts/alias/centroid.sh
@@ -1,5 +1,7 @@
 
 
+# gr4viton.cz/centroid
+
 
 # what's the weather today
 # bring a coat / sunglasses / umbrella
@@ -8,3 +10,16 @@
 # download rick and morty latest with czech subtitles
 # play latest rick and morty on all screens, with sound in combo
 # play rick and morty - default
+
+# MUSIC
+# instant-music-downloader
+
+
+# TRANS DATA
+# download it into mobile
+# where it is previously said content
+# - music / rick and morty
+
+# impl
+# automatic alias creation for all the python functions
+# generate say alias...
--- scripts/alias/git.sh
+++ scripts/alias/git.sh
@@ -4,9 +4,34 @@ alias git_initial_setup='git config --global pull.rebase true; git config --glob
 alias pushf='git push --force-with-lease'
 alias gdiff='git diff --color-words'
 alias gpull_rebase='git pull --rebase origin master'
+
 alias glog='git log --stat'
 alias gsquash2='git rebase --interactive HEAD~2'
 alias g='git'
 alias b='branch'
 alias ch='checkout'
+
+git_set_global() {
+git config --global user.name $1
+git config --global user.email $2
+}
+
+alias git_iam_gr4viton="git_set_global gr4viton lordmutty@gmail.com"
+alias git_iam_kiwi="git_set_global daniel.davidek daniel.davidek@kiwi.com"
+
+recheckout_current_branch() {
+# if the diff is empty 
+# - deletes current branch, pulls origin, checkout it again
+cur=`git rev-parse --abbrev-ref HEAD`
+echo Current branch = $cur
+git checkout master
+git branch -D $cur
+git pull
+git checkout $cur
+}
+
+git_branch() {
+# returns only the last part after '/' of current branch name
+git rev-parse --abbrev-ref HEAD 2>/dev/null | sed 's:.*/::'
+}
 
