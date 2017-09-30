46e00ca3850fbacce90a5aa69c4f3777aa7ec564
gr4viton <lordmutty@gmail.com>
2017-09-30 20:43:46 +0200

    30/09/2017 20:43:46: from

--- scripts/alias/dev.sh
+++ scripts/alias/dev.sh
@@ -6,3 +6,10 @@ make_python_venv () {
 }
 
 alias venv_cv2='source /srv/cv2/venv_cv2/bin/activate'
+
+centroid_dir='/srv/centroid/'
+alias cd_centroid='cd '$centroid_dir
+
+
+# why is import cv2 functional only when venv_cv2 runned from /srv/
+
