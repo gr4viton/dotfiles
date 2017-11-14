df1e7c8351fe572b69887afc2da7da5b021c5611
gr4viton <lordmutty@gmail.com>
2017-11-14 22:35:42 +0100

    14/11/2017 22:35:42: from

--- scripts/alias/dev.sh
+++ scripts/alias/dev.sh
@@ -34,4 +34,55 @@ alias cd_centroid='cd '$centroid_dir
 # why is import cv2 functional only when venv_cv2 runned from /srv/
 
 
-alias mount_ntfs_kill_hibernate='sudo umount /mnt/C || sudo mount -t ntfs-3g -o remove_hiberfile /dev/nvme0n1p3 /mnt/C && echo "/mnt/C remounted Successfully as rw!!"'
+alias mount_ntfs_kill_hibernate_C='sudo umount /mnt/C || sudo mount -t ntfs-3g -o remove_hiberfile /dev/nvme0n1p3 /mnt/C && echo "/mnt/C remounted Successfully as rw!!"'
+alias mount_ntfs_kill_hibernate_D='sudo umount /mnt/D || sudo mount -t ntfs-3g -o remove_hiberfile /dev/nvme0n1p9 /mnt/D && echo "/mnt/D remounted Successfully as rw!!"'
+
+alias mount_C_ro='sudo mount -o ro /dev/nvme0n1p3 /mnt/C'
+alias mount_D_ro='sudo mount -o ro /dev/nvme0n1p9 /mnt/D'
+
+
+recheckout_current_branch() {
+    cur=`git rev-parse --abbrev-ref HEAD`
+    echo Current branch = $cur           
+    git checkout master
+    git branch -D $cur
+    git pull
+    git checkout $cur
+}
+
+# POSTGRES
+
+alias dbm_psql='psql -h 172.17.0.2 -U postgres postgres'
+alias dbm_psql_drop='psql -h 172.17.0.2 -U postgres postgres -c "drop schema public cascade;"'
+alias dbm_psql_create='psql -h 172.17.0.2 -U postgres postgres -c "create schema public;"'
+alias dbm_psql_recreate='dbm_psql_drop; dbm_psql_create'
+
+
+dbm_recheckout_master_alembic_fcn() {
+    cur=`git rev-parse --abbrev-ref HEAD`
+    echo Current branch = $cur           
+    git checkout master
+    cur_master=`git rev-parse --abbrev-ref HEAD`
+    if [ cur_master -ne "master" ]; then
+        echo "You did not switch to master branch!"
+        exit;
+    fi
+    git pull
+    while true; do
+        read -p "Are you sure you are not deleting PRODUCTION DB !???! " yn
+        case $yn in
+            [Yy]* ) dbm_psql_drop; dbm_psql_create; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+    
+    alembic upgrade head
+    git checkout $cur
+}
+
+alias dbm_recheckout_master_alembic='dbm_recheckout_master_alembic_fcn'
+
+
+export WORKON_HOME='~/venvs/'
+source /home/dd/.local/bin/virtualenvwrapper.sh
