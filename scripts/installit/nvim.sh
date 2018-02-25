1bfe4bca1c425f2dc25bcaba1ecc1944be672c4c
gr4viton <lordmutty@gmail.com>
2018-02-25 15:43:43 +0100

    25/02/2018 15:43:43: from dddell-XPS-15 GNU/Linux start

--- scripts/installit/nvim.sh
+++ scripts/installit/nvim.sh
@@ -10,3 +10,23 @@ sudo make install
 
 sudo apt-get install python-neovim
 sudo apt-get install python3-neovim
+
+
+# for the plugins to work
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+pip3 install --user neovim
+
+# install plugins
+#nvim
+#:PlugInstall
+#:UpdateRemotePlugins
+#:q!
+
+
+# set locale
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
