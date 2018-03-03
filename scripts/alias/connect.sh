d534d58d3d10c67a362f96cad434493cc138a365
gr4viton <lordmutty@gmail.com>
2018-03-03 17:27:41 +0100

    ebay md

--- scripts/alias/connect.sh
+++ scripts/alias/connect.sh
@@ -6,3 +6,5 @@ alias dalek_me='ssh pi@192.168.0.198'
 alias octopi_me='ssh pi@192.168.0.111'
 #alias centroid_me='ssh pi@192.168.0.199'
 alias centroid_me='ssh pi@10.8.0.39'
+
+alias tapa_me='ssh gr4viton@192.168.0.118'
